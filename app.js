a8f520ab22da4a428d649ada51124eb10f3ac7d3
thomasbalicki <thomabalic@gmail.com>
2023-05-11 20:56:23 -0500

    initial commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/todolistDB", {
+  .connect("mongodb+srv://thomabalic:Test123@cluster0.8tnncgu.mongodb.net/todolistDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
